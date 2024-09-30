4b2b0503f5d7c45dff6eac6dc1e761c13cf1f812
KarollineSantos <166328907+KarollineSantos@users.noreply.github.com>
2024-09-30 16:10:28 -0300

    Spring Initializr
    Gerando o projeto com as dependências Lombok, Spring Web, MySQL Driver e Spring Data JPA.
    Project: Maven
    Language: Java 21
    Packaging: Jar

---
+++ universidade_crud/src/test/java/com/universidade/universidade_crud/UniversidadeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidade.universidade_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
