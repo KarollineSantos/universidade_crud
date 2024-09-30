d59192f22b0eaea4b316ae25d138ec5379a066a9
Diego Rodrigues <79650470+DiegoRodrigues76@users.noreply.github.com>
2024-09-30 16:51:40 -0300

    Add files via upload

---
+++ universidade_crud/src/src/main/java/com/universidade/universidade_crud/UniversidadeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.universidade.universidade_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversidadeCrudApplication.class, args);
+	}
+
+}
---
+++ universidade_crud/src/src/main/java/com/universidade/universidade_crud/model/Pessoa.java
@@ -0,0 +1,30 @@
+package com.universidade.universidade_crud.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private Integer idade;
+}
